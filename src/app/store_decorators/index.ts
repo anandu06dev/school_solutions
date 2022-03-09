de31fb429fb4ebdb87471ee93a65058041cd1c1e
srinidevlearn <65352160+srinidevlearn@users.noreply.github.com>
2022-03-09 06:09:05 +0530

    updated to relative_pathh
    updated to relative_pathh

--- src/app/store_decorators/index.ts
+++ src/app/store_decorators/index.ts
@@ -1,8 +1,8 @@
-export { Query } from '../store/query.decorator';
-export { setStateByProp } from '../store/update_state.decorator';
-export { setStateByMethod } from '../store/update_state_by_method.decorator';
-export { StoreConfig } from '../store/store.config.decorator';
-// export { Store } from '../store/store.decorator';
-export { CreateAction } from '../store/utility';
-export { resetStore } from '../store/reset.store.decorator';
-export { useTapEffects } from '../store/rxjs.decorator';
+export { Query } from '../store_decorators/query.decorator';
+export { setStateByProp } from '../store_decorators/update_state.decorator';
+export { setStateByMethod } from '../store_decorators/update_state_by_method.decorator';
+export { StoreConfig } from '../store_decorators/store.config.decorator';
+// export { Store } from '../store_decorators/store.decorator';
+export { CreateAction } from '../store_decorators/utility';
+export { resetStore } from '../store_decorators/reset.store.decorator';
+export { useTapEffects } from '../store_decorators/rxjs.decorator';
