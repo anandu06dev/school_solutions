bb82a95ddd4e3b3390d25eae18ccf931ecef497c
srinidevlearn <65352160+srinidevlearn@users.noreply.github.com>
2022-03-12 07:17:11 +0530

    fixed unused resolver
    fixed unused resolver

--- src/app/container/studentdetails/services/studentdetails.resolver.ts
+++ src/app/container/studentdetails/services/studentdetails.resolver.ts
@@ -4,43 +4,13 @@ import {
   RouterStateSnapshot,
   ActivatedRouteSnapshot,
 } from '@angular/router';
-import { initPage } from '@utils/interfaces/page.meta';
 import {
   catchError,
-  concat,
-  concatMap,
-  delay,
   map,
   Observable,
   of,
-  tap,
 } from 'rxjs';
 import { StudentapiService } from './studentapi.service';
-import { StudentFacadeService } from './students.facade.service';
-import { StudentDetailsFacade } from './students.facade_bck';
-
-@Injectable({
-  providedIn: 'root',
-})
-export class StudentdetailsResolver implements Resolve<boolean> {
-  constructor(private facade: StudentFacadeService) {}
-  resolve(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot
-  ): Observable<any> {
-    // return this.facade.getStudentListFacade(initPage).pipe(
-    //   map((d: any) => (d?.data ? d.data : [])),
-    //   concatMap((data: any) => {
-    //     initPage.page = 2;
-    //     return this.facade
-    //       .getStudentListFacade(initPage)
-    //       .pipe(map((d: any) => (d?.data ? d.data : [])));
-    //   })
-    // );
-    return this.facade.getStudentListFacade(initPage);
-  }
-}
-
 @Injectable({
   providedIn: 'root',
 })
